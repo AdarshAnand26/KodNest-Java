5f51b3b75a5237857ced586723d4141ba6560264
Adarsh Anand <adarsh.ad91622@gmail.com>
2026-07-30 13:01:25 +0530

    hello

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,6 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
         System.out.println("Hello AD,\nWelcome to Java Track!!");
+        System.out.println("Now i know how to use git and gihub");
     }
 }
