2c5039d532754009433fddac4ba886419d5be376
Adarsh Anand <adarsh.ad91622@gmail.com>
2026-08-03 12:27:37 +0530

    3Aug

---
+++ track/M01/T01/ST01/P01/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello AD,\nWelcome to Java Track!!");
+        System.out.println("Now i know how to use git and github");
+    }
+}
