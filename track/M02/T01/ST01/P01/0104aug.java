390d69ba72823ed22ec9feea8ec77975e07b006a
Adarsh Anand <adarsh.ad91622@gmail.com>
2026-08-04 11:31:42 +0530

    04Aug

---
+++ track/M02/T01/ST01/P01/0104aug.java
@@ -0,0 +1,14 @@
+class Aug0104 {
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.print("friday");
+        System.out.println("Saturday");
+        System.out.println("sunday");
+
+
+
+    }
+}
